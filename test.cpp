ac90ac0aff78f3934492ecb66e80fb137b74924d
franneck94 <schaffranek.jan@web.de>
2022-06-07 14:37:27 +0200

    up

--- test.cpp
+++ test.cpp
@@ -2,56 +2,90 @@
 
 #include <tlhelp32.h>
 
+#include <cstdint>
 #include <iostream>
 #include <string>
+#include <vector>
 
-DWORD FindProcessId(const std::string &processName)
+void GetAllProcessIdsByName(const std::string targetProcessName, std::vector<DWORD> &pids)
 {
-    PROCESSENTRY32 processInfo;
-    processInfo.dwSize = sizeof(processInfo);
+    HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-    HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-    if (processesSnapshot == INVALID_HANDLE_VALUE)
-    {
-        return 0;
-    }
+    PROCESSENTRY32 entry;
+    entry.dwSize = sizeof entry;
 
-    Process32First(processesSnapshot, &processInfo);
-    if (!processName.compare(processInfo.szExeFile))
-    {
-        CloseHandle(processesSnapshot);
-        return processInfo.th32ProcessID;
-    }
+    if (!Process32First(snap, &entry))
+        return;
 
-    while (Process32Next(processesSnapshot, &processInfo))
+    do
     {
-        if (!processName.compare(processInfo.szExeFile))
+        if (std::string(entry.szExeFile) == targetProcessName)
         {
-            CloseHandle(processesSnapshot);
-            return processInfo.th32ProcessID;
+            pids.emplace_back(entry.th32ProcessID);
         }
-    }
+    } while (Process32Next(snap, &entry));
+}
 
-    CloseHandle(processesSnapshot);
-    return 0;
+bool ReadWideString(HANDLE hProc, std::uintptr_t addr, std::wstring &out)
+{
+    out.resize(100);
+    SIZE_T numRead = 0;
+    if (!ReadProcessMemory(hProc, reinterpret_cast<LPVOID>(addr), &out[0], 100 * sizeof(wchar_t), &numRead))
+    {
+        out.clear();
+        return false;
+    }
+    out.resize(numRead / sizeof(wchar_t));
+    return true;
+}
+
+bool tryParse(std::string &input, int &output)
+{
+    try
+    {
+        output = std::stoi(input);
+    }
+    catch (std::invalid_argument)
+    {
+        return false;
+    }
+    return true;
 }
 
 int main()
 {
-    const auto ProcessId = FindProcessId("Gw.exe");
-    std::cout << "ProcessId: " << ProcessId << std::endl;
-    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessId);
+    std::vector<DWORD> pids;
+    GetAllProcessIdsByName("Gw.exe", pids);
 
-    SIZE_T NumberOfBytesRead;
-    constexpr auto size = 4U;
-    char Buffer[size] = {0};
-    const auto dwAddr = 0x016C5411;
-    BOOL success = ReadProcessMemory(hProcess, &dwAddr, &Buffer, size * sizeof(char), &NumberOfBytesRead);
+    for (int i = 0; i < pids.size(); ++i)
+    {
+        std::cout << i << " - GW Process ID: " << pids[i] << '\n';
+    }
+    DWORD pid = 0;
+    std::cout << "Select GW Process: ";
 
-    if (!success)
-        return 1;
+    std::string input;
+    int idx;
+    getline(std::cin, input);
+    while (!tryParse(input, idx))
+    {
+        std::cout << "Bad entry. Enter a NUMBER: ";
+        getline(std::cin, input);
+    }
+    pid = pids[idx];
 
-    std::cout << Buffer << std::endl;
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+
+    uint64_t size = 100;
+    char *message = new char[size]{};
+    const auto num_bytes = size * sizeof(char);
+
+    std::uintptr_t dwAddr = 0x010892F0;
+
+    std::wstring w(100, L'\0');
+    ReadWideString(hProcess, dwAddr, w);
+    std::string s(w.begin(), w.end());
+    std::cout << s << std::endl;
 
     return 0;
 }
