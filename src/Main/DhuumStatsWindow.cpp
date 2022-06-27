c4f9667e47ae71aa652ee52c0229484861d8644f
franneck94 <schaffranek.jan@web.de>
2022-06-27 10:52:02 +0200

    added dhuum stats code

--- src/Main/DhuumStatsWindow.cpp
+++ src/Main/DhuumStatsWindow.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 
+#include <algorithm>
 #include <array>
 #include <cstdint>
 
@@ -31,13 +32,99 @@ void DhuumStatsWindow::Draw(IDirect3DDevice9 *pDevice)
     if (!player.ValidateData(UwHelperActivationConditions))
         return;
 
-    ImGui::SetNextWindowSize(ImVec2(125.0F, 50.0F), ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowSize(ImVec2(170.0F, 175.0F), ImGuiCond_FirstUseEver);
 
-    if (ImGui::Begin("DhuumStatsWindow", nullptr, GetWinFlags() | ImGuiWindowFlags_NoDecoration))
+    if (ImGui::Begin("DhuumStatsWindow", nullptr, GetWinFlags() | ImGuiWindowFlags_NoScrollbar))
     {
         const auto width = ImGui::GetWindowWidth();
+
+        ImGui::Text("Dhuum HP: %3.0f%%", dhuum_hp * 100.0F);
+        const auto timer_ms = TIMER_DIFF(dhuum_fight_start_time_ms);
+        ImGui::Text("Timer: %4.0f", static_cast<float>(timer_ms) / 1000.0F);
+        ImGui::Separator();
+        ImGui::Text("Rests: %u", num_casted_rest);
+        ImGui::Text("Rests (per s): %0.2f", rests_per_s);
+        ImGui::Text("ETA Rest (s): %0.2f", (num_casted_rest > 0 && eta_rest < 10'000.0F) ? eta_rest : 0.0F);
+        ImGui::Separator();
+        ImGui::Text("Num Attacks: %u", num_attacks);
+        ImGui::Text("Damage (per s): %0.2f", damage_per_s);
+        ImGui::Text("ETA Damage (s): %0.2f", (num_attacks > 0 && eta_damage < 10'000.0F) ? eta_damage : 0.0F);
     }
     ImGui::End();
+}
+
+void DhuumStatsWindow::ResetData()
+{
+    dhuum_fight_active = true;
+    dhuum_fight_start_time_ms = clock();
+
+    num_casted_rest = 0U;
+    rests_per_s = 0.0F;
+    eta_rest = 0.0F;
+    rests.clear();
+
+    num_attacks = 0U;
+    damage_per_s = 0.0F;
+    eta_damage = 0.0F;
+    damages.clear();
+}
+
+void DhuumStatsWindow::RemoveOldData()
+{
+    const auto remove_rest_it = std::remove_if(rests.begin(), rests.end(), [=](const auto t) {
+        return std::abs(TIMER_DIFF(t)) > TIME_WINDOW_MS;
+    });
+    rests.erase(remove_rest_it, rests.end());
+
+    const auto remove_dmg_it = std::remove_if(damages.begin(), damages.end(), [=](const auto p) {
+        return std::abs(TIMER_DIFF(p.first)) > TIME_WINDOW_MS;
+    });
+    damages.erase(remove_dmg_it, damages.end());
+}
+
+void DhuumStatsWindow::UpdateRestData()
+{
+    const auto current_time_ms = clock();
+
+    auto idx_rests = uint32_t{0};
+    for (const auto time : rests)
+    {
+        if (time >= current_time_ms - TIME_WINDOW_MS)
+            ++idx_rests;
+    }
+
+    if (idx_rests > 0)
+        rests_per_s = static_cast<float>(idx_rests) / static_cast<float>(TIME_WINDOW_S);
+    else
+        rests_per_s = 0.0F;
+
+    const auto still_needed_rest = NEEDED_NUM_REST - num_casted_rest;
+    eta_rest = still_needed_rest / (rests_per_s + FLT_EPSILON);
+}
+
+void DhuumStatsWindow::UpdateDamageData()
+{
+    const auto current_time_ms = clock();
+
+    auto idx_dmg = uint32_t{0};
+    for (const auto [time, dmg] : damages)
+    {
+        if (time >= current_time_ms - TIME_WINDOW_MS)
+        {
+            damage_per_s += dmg;
+            ++idx_dmg;
+        }
+    }
+
+    if (idx_dmg > 0)
+        damage_per_s /= static_cast<float>(TIME_WINDOW_S);
+    else
+        damage_per_s = 0.0F;
+
+    if (dhuum_hp >= 0.25F && dhuum_hp < 1.0F && damage_per_s > 0.0F)
+        eta_damage = ((dhuum_max_hp * dhuum_hp) - (dhuum_max_hp * 0.25)) / damage_per_s;
+    else
+        eta_damage = 0.0F;
 }
 
 void DhuumStatsWindow::Update(float delta)
@@ -47,4 +134,35 @@ void DhuumStatsWindow::Update(float delta)
     if (!player.ValidateData(UwHelperActivationConditions))
         return;
     player.Update();
+
+    const auto is_in_dhuum_room = IsInDhuumRoom(player);
+    if (!is_in_dhuum_room)
+    {
+        dhuum_fight_active = false;
+        return;
+    }
+
+    const auto is_in_dhuum_fight = IsInDhuumFight(&dhuum_id, &dhuum_hp, &dhuum_max_hp);
+    if (!is_in_dhuum_fight || !dhuum_id)
+    {
+        dhuum_fight_active = false;
+        return;
+    }
+
+    if (dhuum_hp == 1.0F)
+    {
+        dhuum_fight_active = false;
+        return;
+    }
+
+    // Fight started, reset all data
+    if (!dhuum_fight_active)
+    {
+        ResetData();
+        return;
+    }
+
+    RemoveOldData();
+    UpdateRestData();
+    UpdateDamageData();
 }
--- src/Main/DhuumStatsWindow.h
+++ src/Main/DhuumStatsWindow.h
@@ -7,6 +7,7 @@
 #include <GWCA/GameEntities/Agent.h>
 
 #include <Actions.h>
+#include <Callbacks.h>
 #include <Player.h>
 #include <Types.h>
 
@@ -15,7 +16,124 @@
 class DhuumStatsWindow : public HelperBoxWindow
 {
 public:
-    DhuumStatsWindow() : player({}){};
+#ifdef _DEBUG
+    static constexpr auto REST_SKILL_ID = static_cast<uint32_t>(GW::Constants::SkillID::Reversal_of_Fortune);
+#else
+    static constexpr auto REST_SKILL_ID = uint32_t{GW::Constants::SkillID::Reversal_of_Fortune};
+#endif
+    static constexpr auto NEEDED_NUM_REST = uint32_t{600U};
+
+private:
+    void SkillPacketCallback(const uint32_t value_id,
+                             const uint32_t caster_id,
+                             const uint32_t target_id,
+                             const uint32_t value,
+                             const bool no_target)
+    {
+        uint32_t agent_id = caster_id;
+        const uint32_t activated_skill_id = value;
+
+        // ignore non-skill packets
+        switch (value_id)
+        {
+        case GW::Packet::StoC::GenericValueID::instant_skill_activated:
+        case GW::Packet::StoC::GenericValueID::skill_activated:
+        case GW::Packet::StoC::GenericValueID::skill_finished:
+        case GW::Packet::StoC::GenericValueID::attack_skill_activated:
+        case GW::Packet::StoC::GenericValueID::attack_skill_finished:
+        {
+            if (!no_target)
+            {
+                agent_id = target_id;
+            }
+            break;
+        }
+        default:
+        {
+            return;
+        }
+        }
+
+        if (REST_SKILL_ID == activated_skill_id)
+        {
+            ++num_casted_rest;
+            rests.push_back(clock());
+        }
+    }
+
+    void DamagePacketCallback(const uint32_t type,
+                              const uint32_t caster_id,
+                              const uint32_t target_id,
+                              const float value)
+    {
+        // ignore non-damage packets
+        switch (type)
+        {
+        case GW::Packet::StoC::P156_Type::damage:
+        case GW::Packet::StoC::P156_Type::critical:
+        case GW::Packet::StoC::P156_Type::armorignoring:
+            break;
+        default:
+            return;
+        }
+
+        // ignore heals
+        if (value >= 0)
+            return;
+
+        const auto agents = GW::Agents::GetAgentArray();
+        if (!agents.valid() || !caster_id)
+            return;
+
+        const auto cause = agents[caster_id]->GetAsAgentLiving();
+        if (!cause)
+            return;
+        if (cause->allegiance != static_cast<uint8_t>(GW::Constants::Allegiance::Ally_NonAttackable) &&
+            cause->allegiance != static_cast<uint8_t>(GW::Constants::Allegiance::Npc_Minipet))
+            return;
+
+        const auto target = agents[target_id]->GetAsAgentLiving();
+        if (!target || target->agent_id != dhuum_id)
+            return;
+
+        long ldmg = std::lround(-value * target->max_hp);
+        const uint32_t dmg = static_cast<uint32_t>(ldmg);
+        ++num_attacks;
+
+        const auto time = clock();
+        damages.push_back(std::make_pair(time, dmg));
+    }
+
+    void ResetData();
+    void RemoveOldData();
+    void UpdateRestData();
+    void UpdateDamageData();
+
+public:
+    DhuumStatsWindow() : player({}), rests({}), damages({})
+    {
+        /* Skill on self or party player */
+        GW::StoC::RegisterPacketCallback<GW::Packet::StoC::GenericValue>(
+            &SkillCasted_Entry,
+            [this](GW::HookStatus *, GW::Packet::StoC::GenericValue *packet) -> void {
+                const uint32_t value_id = packet->Value_id;
+                const uint32_t caster_id = packet->agent_id;
+                const uint32_t target_id = 0U;
+                const uint32_t value = packet->value;
+                const bool no_target = true;
+                SkillPacketCallback(value_id, caster_id, target_id, value, no_target);
+            });
+
+        GW::StoC::RegisterPacketCallback<GW::Packet::StoC::GenericModifier>(
+            &Damage_Entry,
+            [this](GW::HookStatus *, GW::Packet::StoC::GenericModifier *packet) -> void {
+                const uint32_t type = packet->type;
+                const uint32_t caster_id = packet->cause_id;
+                const uint32_t target_id = packet->target_id;
+                const float value = packet->value;
+                DamagePacketCallback(type, caster_id, target_id, value);
+            });
+    };
     ~DhuumStatsWindow(){};
 
     static DhuumStatsWindow &Instance()
@@ -50,4 +168,27 @@ public:
 
 private:
     Player player;
+
+    GW::HookEntry SkillCasted_Entry;
+    GW::HookEntry Damage_Entry;
+
+    const long TIME_WINDOW_S = 10L;
+    const long TIME_WINDOW_MS = (TIME_WINDOW_S * 1000L);
+
+    uint32_t dhuum_id = 0U;
+    float dhuum_hp = 0.0F;
+    uint32_t dhuum_max_hp = 0U;
+
+    long dhuum_fight_start_time_ms = 0L;
+    bool dhuum_fight_active = false;
+
+    uint32_t num_casted_rest = 0U;
+    float rests_per_s = 0.0F;
+    float eta_rest = 0.0F;
+    std::vector<long> rests;
+
+    uint32_t num_attacks = 0U;
+    float damage_per_s = 0.0F;
+    float eta_damage = 0.0F;
+    std::vector<std::pair<long, float>> damages;
 };
