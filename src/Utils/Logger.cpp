0de8c9c3b8df8c3b82d0bf8cc80d2c4f90bf8c52
franneck94 <schaffranek.jan@web.de>
2022-07-11 08:53:46 +0200

    refactor

--- src/Utils/Logger.cpp
+++ src/Utils/Logger.cpp
@@ -1,3 +1,9 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdio>
+#include <ctime>
+#include <string>
+
 #include <GWCA/GameContainers/Array.h>
 #include <GWCA/Managers/ChatMgr.h>
 #include <GWCA/Managers/GameThreadMgr.h>
@@ -25,8 +31,6 @@ enum LogType : uint8_t
     LogType_Warning,
     LogType_Error
 };
-
-auto crash_dumped = false;
 } // namespace
 
 static void GWCALogHandler(void *, GW::LogLevel, const char *msg, const char *, unsigned int, const char *)
--- src/Utils/Logger.h
+++ src/Utils/Logger.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "stdafx.h"
-
 #include <GWCA/Managers/ChatMgr.h>
 
 namespace Log
