db1cb893f7e9578a9f7f6383e040c30354e25ecc
franneck94 <schaffranek.jan@web.de>
2022-07-12 14:10:51 +0200

    removed includes

--- src/stdafx.h
+++ src/stdafx.h
@@ -2,24 +2,10 @@
 
 #include <functional>
 
-// windows headers
 #include <Windows.h>
-
-#include <DbgHelp.h>
-#include <ShlObj.h>
-#include <Shlwapi.h>
-#include <TlHelp32.h>
-#include <WS2tcpip.h>
-#include <WinInet.h>
-#include <WinSock2.h>
-#include <WinUser.h>
-#include <shellapi.h>
-#include <strsafe.h>
 #include <windowsx.h>
 
 #include <GWCA/GameContainers/Array.h>
-#include <GWCA/GameContainers/GamePos.h>
-#include <GWCA/GameContainers/List.h>
 
 #pragma warning(disable : 4201)
 #pragma warning(disable : 4505)
