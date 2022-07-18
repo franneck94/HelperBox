78420c3863149d5e6cab79d0546484135076568b
franneck94 <schaffranek.jan@web.de>
2022-07-18 19:11:04 +0200

    remove comment

--- src/Main/Base/SettingsWindow.cpp
+++ src/Main/Base/SettingsWindow.cpp
@@ -80,7 +80,7 @@ bool SettingsWindow::DrawSettingsSection(const char *section)
     if (icon)
     {
         const auto &style = ImGui::GetStyle();
-        const auto text_offset_x = ImGui::GetTextLineHeightWithSpacing() + 4.0f; // TODO: find a proper number
+        const auto text_offset_x = ImGui::GetTextLineHeightWithSpacing() + 4.0f;
         ImGui::GetWindowDrawList()->AddText(ImVec2(pos.x + text_offset_x, pos.y + style.ItemSpacing.y / 2),
                                             ImColor(style.Colors[ImGuiCol_Text]),
                                             icon);
