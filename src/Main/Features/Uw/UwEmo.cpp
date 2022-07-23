51b8cf32388c39247c8e7c243df0ae899a909953
franneck94 <schaffranek.jan@web.de>
2022-07-23 21:36:18 +0200

    updated re-activate params

--- src/Main/Features/Uw/UwEmo.cpp
+++ src/Main/Features/Uw/UwEmo.cpp
@@ -834,8 +834,8 @@ void EmoRoutine::Update()
         action_state = ActionState::ACTIVE;
     }
 
-    if (action_state == ActionState::INACTIVE && player_data->living && player_data->living->GetIsIdle() &&
-        ((player_data->energy_perc < 0.25F || player_data->hp_perc < 0.25F) && livings_data->enemies.size() == 1U))
+    if (action_state == ActionState::INACTIVE && player_data->living && !player_data->living->GetIsMoving() &&
+        ((player_data->energy_perc < 0.10F || player_data->hp_perc < 0.10F) && livings_data->enemies.size() > 0U))
         action_state = ActionState::ACTIVE;
 
     if (action_state == ActionState::ACTIVE)
